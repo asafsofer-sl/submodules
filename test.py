fd5829cd3d17639a680d0497aed4b60a7b77b241
Asaf Sofer <59789037+asafsofer-sl@users.noreply.github.com>
2026-04-05 11:26:18 +0300

    Change remote queue name to 'exampleQueue'

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from allegroai import Task
 
 task = Task.init(project_name='runai/devops', task_name='Assaf plotly')
 task.set_base_docker("nvidia/cuda:11.1.1-base-ubuntu20.04")
-task.execute_remotely(queue_name="default")
+task.execute_remotely(queue_name="exampleQueue")
 
 gen_plot.print_plotly()
